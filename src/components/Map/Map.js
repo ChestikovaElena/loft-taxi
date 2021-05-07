9230b33e11a3245e6a82f2d9f7f33b3398f74303
Elena Chestikova <Chestikova.elena@gmail.com>
2021-05-07 23:37:40 +0500

    refactor(map): add interactive map
    add interactive map using mapbox-gl

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import mapboxgl from 'mapbox-gl';
 
-const Map = () => {
-  return <h2 className="page__title">Карта</h2>
+// const createMap = container => {
+//   mapboxgl.accessToken = 'pk.eyJ1IjoiZWxlbmFjaCIsImEiOiJja29jdXV4ZHIxcTU2Mnlxa3l2enc0cTdhIn0.f4jSaqU4WJP6C9ILIK3rkQ';
+//   const myMap = new mapboxgl.Map({
+//     container: container,
+//     style: 'mapbox://styles/mapbox/basic-v9',
+//     center: [37.78, 56.51],
+//     zoom: 11
+//   });
+//   return myMap;
+// };
+
+class Map extends React.Component{
+  map = null;
+  mapContainer = React.createRef();
+
+  componentDidMount() {
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiZWxlbmFjaCIsImEiOiJja29jdXV4ZHIxcTU2Mnlxa3l2enc0cTdhIn0.f4jSaqU4WJP6C9ILIK3rkQ';
+
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer.current,
+      style: 'mapbox://styles/mapbox/basic-v9',
+      center: [56.25, 58.00],
+      zoom: 13,
+    });
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
+  render() {
+    return (
+      <div className="map-wrapper">
+        <div data-testid="map" className="map" ref={this.mapContainer}></div>
+      </div>
+    );
+  };
 }
 
 export { Map };
