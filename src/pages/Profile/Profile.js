f1426fb035e40983ae90fa4d18196edc5e800439
Elena Chestikova <Chestikova.elena@gmail.com>
2021-05-17 02:04:05 +0500

    refactor(reducers): add tests for reducers
    add tests for reducer

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ProfileForm from '../../components/ProfileForm';
-import ProfileWarning from '../../components/ProfileWarning';
-import Header from '../../components/Header';
 import Map from '../../components/Map';
 
 export class Profile extends React.Component {
   render() {
     return (
       <>
-        <Header />
         <Map />
         <div className='overlay'>
           <ProfileForm />
---
+++ src/store/reducers/card.spec.js
@@ -0,0 +1,27 @@
+import card from './card';
+import {getCard, getCardSuccess, getCardFailure} from '../actions/card';
+
+describe("card", () => {
+  describe("#GET_CARD", () => {
+    it('returns isLoadding true', () => {
+      expect(card({}, getCard())).toEqual({
+        "isLoadding": true});
+    });
+  });
+
+  describe("#GET_CARD_SUCCESS", () => {
+    it('returns isLoadding false', () => {
+      expect(card({}, getCardSuccess())).toEqual({
+        "isLoadding": false});
+    });
+  });
+
+  describe("#GET_CARDS_FAILURE", () => {
+    it('returns isLoggedIn false', () => {
+      expect(card({}, getCardFailure())).toEqual({
+        "isLoggedIn": false,
+        "error": undefined,
+        "isLoadding": false});
+    });
+  });
+});
